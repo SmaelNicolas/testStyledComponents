eb3fcb206910b51a4c9a83f1cd70f192747fc8b4
Smael Nicolas <nico.smael@gmail.com>
2022-01-20 21:34:21 -0300

    tests useEffect y props

--- src/components/ButtonTest/ButtonTest.jsx
+++ src/components/ButtonTest/ButtonTest.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 
 const Button = styled.button`
@@ -9,8 +10,32 @@ const Button = styled.button`
 	text-decoration: none;
 `;
 
+const Counter = styled.span`
+	border: 2px solid green;
+	font-style: italic;
+	padding: 40px;
+	font-size: ${(props) => props.size || "50px"};
+	color: ${(props) => props.color || "white"};
+`;
+
 function ButtonTest() {
-	return <Button>Test Button</Button>;
+	const [numero, setNumero] = useState(0);
+
+	useEffect(() => {}, [numero]);
+	const sumar = () => {
+		setNumero(numero + 1);
+		console.log(numero);
+	};
+
+	return (
+		<>
+			<Button onClick={sumar}>Test Button</Button>;
+			<Counter color='red' size='30'>
+				{numero}
+			</Counter>
+			<Counter>{numero}</Counter>
+		</>
+	);
 }
 
 export default ButtonTest;
